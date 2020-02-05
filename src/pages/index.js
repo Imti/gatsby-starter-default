0087ca92520d33111af03e8234a7a77fa8b4f0b1
Imti <imtiazmaj@gmail.com>
2020-02-04 17:38:27 -0800

    Add basic netlify function

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,27 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello world!</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  componentDidMount() {
+    fetch("/.netlify/functions/token-hider")
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hello world!</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage
